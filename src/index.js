a528b3eb6f484fd8a901f5798723e392bc69024b
davidebalice <davide.balice@gmail.com>
2023-08-01 20:48:49 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Header from './common/Header';
-import Calculator from './components/Calculator';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./common/Header";
+import Calculator from "./components/Calculator";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Header />
     <Calculator />
   </React.StrictMode>
 );
-
 
 reportWebVitals();
