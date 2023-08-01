4035a7f94a64bf29d60fa7a654ac9204c5e6fc06
davidebalice <davide.balice@gmail.com>
2023-08-01 22:07:22 +0200

    update

--- src/common/Header.js
+++ src/common/Header.js
@@ -4,27 +4,25 @@ import classes from "./Header.module.css";
 const Header = () => {
   return (
     <>
-        <header className={classes.header}>
-          <div className="header-area">
-            <div className="main-header header-sticky">
-              <div className="container-fluid">
-                <div className="row align-items-center">
-                  <div className="col-xl-2 col-lg-2 col-md-1">
-                    <div className={classes.logoContainer}>
-                        <img
-                          src={
-                            window.location.origin + "/assets/img/logo/logo.png"
-                          }
-                          alt="db logo"
-                          className={classes.logo}
-                        />
-                    </div>
+      <header className={classes.header}>
+        <div className="header-area">
+          <div className="main-header header-sticky">
+            <div className="container-fluid">
+              <div className="row align-items-center">
+                <div className="col-xl-2 col-lg-2 col-md-1">
+                  <div className={classes.logoContainer}>
+                    <img
+                      src="./../assets/img/logo/logo.png"
+                      alt="db logo"
+                      className={classes.logo}
+                    />
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </header>
+        </div>
+      </header>
     </>
   );
 };
